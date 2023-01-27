ff2355da7a5b4482216790de35c0601ccb42bc56
Tamani Phiri <tamanigabriel0@gmail.com>
2023-01-27 12:57:35 -0800

    done Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,46 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons,Span } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header Portfolio 
-    <img src="./TroiLogo.png"/>
-  </div>
+  <Container>
+      <Div1>
+          <Link href="/">
+            <a style={{ display:"flex", alignItems:"center", color:"#fff", marginBottom:'18px'}}>
+              <DiCssdeck size="3rem"/><Span>TamaniFolio</Span>
+            </a>
+          </Link>
+      </Div1>
+      <Div2>
+        <li>
+          <Link href="#projects">
+            <NavLink>Projects</NavLink>
+          </Link>
+        </li>
+        <li>
+          <Link href="#tech">
+            <NavLink>Tech</NavLink>
+          </Link>
+        </li>
+        <li>
+          <Link href="#About">
+            <NavLink>About</NavLink>
+          </Link>
+        </li>
+      </Div2>
+      <Div3>
+        <SocialIcons href="https://github.com/TamaniPhiri">
+          <AiFillGithub size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/tamani-phiri-389b16256/">
+          <AiFillLinkedin size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href="https://www.instagram.com">
+          <AiFillInstagram size="3rem"/>
+        </SocialIcons>
+      </Div3>
+  </Container>
 );
 
 export default Header;

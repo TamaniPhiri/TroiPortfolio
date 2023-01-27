75f609acc899c92316caed51f04cd85d4b7f6ec7
Tamani Phiri <tamanigabriel0@gmail.com>
2023-01-27 14:05:40 -0800

    done project section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome to<br/>
+          Tamani's Portfolio
+        </SectionTitle>
+        <SectionText>
+        As a Junior Full Stack Developer with a passion for creating seamless web and mobile experiences, I specialize in utilizing React, Node, Express, HTML, CSS, Mongo DB, Adobe XD and Figma to bring my clients' visions to life. My skills in these technologies, along with my expertise in React Native, allow me to build dynamic and user-friendly applications that stand out in today's digital landscape.
+        </SectionText>
+        <Button onClick={()=>window.location='https://www.linkedin.com/in/tamani-phiri-389b16256/'}>Learn More</Button>
+      </LeftSection>
+  </Section>
 );
 
 export default Hero;
